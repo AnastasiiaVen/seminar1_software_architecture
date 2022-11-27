5875a51e0f50a34df3892f7a77477835a2a65226
AnastasiiaVen <95999712+AnastasiiaVen@users.noreply.github.com>
2022-11-27 17:32:37 +0300

    Update Camera.java

--- src/main/java/ru/geekbrains/seminar1/modelelements/Camera.java
+++ src/main/java/ru/geekbrains/seminar1/modelelements/Camera.java
@@ -4,25 +4,36 @@ import java.util.Collection;
 
 public class Camera {
 
-    private Collection<Point3D> location;
-    private Collection<Angle3D> angle;
+    private Point3D location;
+    private Angle3D angle;
 
-    public Collection<Point3D> getLocation() {
+    public Point3D getLocation() {
         return location;
     }
 
-    public Collection<Angle3D> getAngle() {
+    public void setLocation(Point3D location) {
+        this.location = location;
+    }
+
+    public Angle3D getAngle() {
         return angle;
     }
 
-    public Camera(Collection<Point3D> location, Collection<Angle3D> angle) {
-        this.location = location;
+    public void setAngle(Angle3D angle) {
         this.angle = angle;
     }
+    public Camera(Point3D point3D, Angle3D angle3D) {
+        this.location = point3D;
+        this.angle = angle3D;
+    }
 
-    public static void Rotate(Angle3D angle3D){}
+    public static void Rotate(Angle3D angle3D){
+        this.angle = angle3D;
+    }
 
-    public static void Move(Point3D point3D){}
+    public static void Move(Point3D point3D){
+        this.location = point3D;
+    }
 
 
 }
