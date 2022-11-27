295314469fd56c8d761508a44a182a295cf3158a
AnastasiiaVen <95999712+AnastasiiaVen@users.noreply.github.com>
2022-11-27 17:28:47 +0300

    Update Flash.java

--- src/main/java/ru/geekbrains/seminar1/modelelements/Flash.java
+++ src/main/java/ru/geekbrains/seminar1/modelelements/Flash.java
@@ -4,28 +4,46 @@ import java.util.Collection;
 
 public class Flash {
 
-    private Collection<Point3D> location;
-    private Collection<Angle3D> angle;
+    private Point3D location;
+    private Angle3D angle;
     private Color color;
     private Float power;
 
-    public Collection<Point3D> getLocation() {
+
+    public Point3D getLocation() {
         return location;
     }
 
-    public Collection<Angle3D> getAngle() {
+    public void setLocation(Point3D location) {
+        this.location = location;
+    }
+
+    public Angle3D getAngle() {
         return angle;
+    }
+
+    public void setAngle(Angle3D angle) {
+        this.angle = angle;
     }
 
     public Color getColor() {
         return color;
     }
 
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
     public Float getPower() {
         return power;
     }
 
-    public Flash(Collection<Point3D> location, Collection<Angle3D> angle, Color color, Float power) {
+    public void setPower(Float power) {
+        this.power = power;
+    }
+
+
+    public Flash(Point3D location, Angle3D angle, Color color, Float power) {
         this.location = location;
         this.angle = angle;
         this.color = color;
